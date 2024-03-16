13d061446578dfc20990e123b5ac4f3cdb525057
Sishel <sishel@Sishels-MacBook-Air.local>
2024-03-16 18:23:36 +0100

    LeetCode problem 167

--- Problems/TwoSumSorted167.py
+++ Problems/TwoSumSorted167.py
@@ -1,3 +1,26 @@
 """
 Solving using a two-pointer approach
+
 """
+
+class solution:
+	def twoSum(self, numbers, target):
+		left, rigth = 0, len(numbers)-1
+
+		while left < right:
+			toatl = numbers[left] + numbers[right]
+
+			if total == target:
+				return [left + 1, right + 1,]
+			elif total < target:
+				left += 1
+			else:
+				right -= 1
+
+		return target
+
+
+
+
+# S: O(1)  the solution only requires a few integer variables (left, right, total)
+# T: O(n)
