4d4e9b827809d10f37c8bab6cb188e4f4ec1218c
Sishel <sishel@Sishels-MacBook-Air.local>
2024-04-02 16:50:26 +0200

    LeetCode problem 654

--- MaximumBinaryTree654.py
+++ MaximumBinaryTree654.py
@@ -17,3 +17,15 @@ class Solution:
 		root.right = self.constructMaximumBinaryTree(nums[max_idx+1:])
 
 		return root
+
+
+# recursively building the max binary tree by iterating over each element in the input list nums.
+# At each level of recursion, the function performs a linear search to find the max val in the current sublist, which takes O(n) time
+# at each level, the size of the sublist is reduced by approximately half (assuming the elements are evenly distributed)!
+# T: O(n logn)
+
+# the recursion stack space used during the recursive calls
+# At each recursive call, additional stack space is used to store the function call and local variables, including the sublist of nums.
+# the function is called recursively for each partitioned sublist --> max depth of recursion: O(log n).
+# S: O(logn)
+
