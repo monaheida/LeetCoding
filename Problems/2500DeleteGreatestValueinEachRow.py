5c7a78ff56c8b6e5b6f770363394eb5c5200c631
Sishel <sishel@Sishels-MacBook-Air.local>
2024-04-18 09:42:14 +0200

    LeetCode problem 2500

--- Problems/2500DeleteGreatestValueinEachRow.py
+++ Problems/2500DeleteGreatestValueinEachRow.py
@@ -1,2 +1,16 @@
+"""
+zip(*grid): transposes the grid.
+*grid: unpack the grid, passing each row of the grid as a separate argument to zip().
+zip() then pairs up the elements at corresponding positions from each row, effectively transposing the grid.
+
+"""
+
+from typing import List
+
 class Solution:
 	def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+		for i in range(len(grid)):
+			gird[i].sort()
+		grid = list(zip(*grid))
+
+		return sum(max(row) for row in grid)
