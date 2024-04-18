4cb2c02dfe0233afba86970a9c0d781e826cd002
Sishel <sishel@Sishels-MacBook-Air.local>
2024-04-18 13:43:54 +0200

    LeetCode problem 1337

--- Problems/1337TheKWeakestRowsinaMatrix.py
+++ Problems/1337TheKWeakestRowsinaMatrix.py
@@ -1,4 +1,6 @@
 """
+Solved by iterating through each row, counting the number of soldiers,
+and then sorting the rows based on the number of soldiers and their indices.
 
 """
 
@@ -6,4 +8,9 @@ from typing import List
 
 class Solution:
 	def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+		soldiers_count = [(sum(row), i) for i, row in enumerate(mat)]
+		soldiers_count.sort()
+
+		return [i for _, i in soldiers_count[:k]]
+
 
