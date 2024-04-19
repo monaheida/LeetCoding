de9781af7f04ab90f89dec8a81018ef93b8a0117
Sishel <sishel@Sishels-MacBook-Air.local>
2024-04-19 13:24:30 +0200

    LeetCode problem 2357

---
+++ Problems/2357MakeArrayZerobySubtractingEqualAmounts.py
@@ -0,0 +1,13 @@
+"""
+ Counting the number of unique non-zero elements in the array -->  effectively represents 
+ the minimum number of operations required to make all elements zero.
+- converts the input list "nums" to a set to remove duplicate elements.
+- subtracts the {0} set from the set of unique elements, effectively removing the zero element.
+- returns the length of the resulting set --> the count of unique non-zero elements.
+
+"""
+from typing import List
+
+class Solution:
+	def minimumOperations(self, nums: List[int]) -> int:
+		return len(set(nums) - {0})
