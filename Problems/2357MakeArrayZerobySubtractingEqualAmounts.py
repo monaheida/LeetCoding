130b041d34e583e09e392a9a05d9d45c43eee8fd
Sishel <sishel@Sishels-MacBook-Air.local>
2024-04-21 08:57:05 +0200

    LeetCode problem 2336

--- Problems/2357MakeArrayZerobySubtractingEqualAmounts.py
+++ Problems/2357MakeArrayZerobySubtractingEqualAmounts.py
@@ -5,6 +5,8 @@
 - subtracts the {0} set from the set of unique elements, effectively removing the zero element.
 - returns the length of the resulting set --> the count of unique non-zero elements.
 
+* Sets in Python do not allow duplicate elements, so any duplicate elements in the list will be removed. 
+
 """
 from typing import List
 
