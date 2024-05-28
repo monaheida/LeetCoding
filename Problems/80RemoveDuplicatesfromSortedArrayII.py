0acaa42034abf17f2432004ab4c5efaf27b4fb66
monaheida <sishel.heidari@gmail.com>
2024-05-28 15:08:39 +0200

    LeetCode problem 80

---
+++ Problems/80RemoveDuplicatesfromSortedArrayII.py
@@ -0,0 +1,11 @@
+
+from typing import List
+
+class Solution:
+	def removeDuplicates(self, nums: List[int]) -> int:
+		k = 0
+		for i in nums:
+			if k < 2 or i != nums[k - 2]:
+				nums[k] = i
+				k += 1
+		return k
